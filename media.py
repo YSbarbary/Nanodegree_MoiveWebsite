551b951c3680c24a371dc8f87d28554f04abec5b
yasser888 <yasser.mis@ci.menofia.edu.eg>
2015-07-08 17:57:49 +0200

    Update media.py

--- media.py
+++ media.py
@@ -14,10 +14,14 @@ class Movie():
                  movie_rating,
                  poster_image,
                  trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.directors = movie_directors
+		self.writer = movie_writer
+		self.stars = movie_stars
+		self.rating = movie_rating
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
